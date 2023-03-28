d4f0d59008cc06873dc7a66b5650f1952f2af6a4
theusaf <theusafyt@gmail.com>
2023-03-28 10:23:29 -0700

    feat(wanikani): create typings for body controller

---
+++ src/types/wanikani/controllers/body_controller.d.ts
@@ -0,0 +1,16 @@
+declare module "controllers/body_controller" {
+  import { Controller } from "@hotwired/stimulus";
+  export default class extends Controller {
+    static classes: string[];
+    /**
+     * Freezes the body.
+     */
+    freeze(): void;
+    /**
+     * Unfreezes the body.
+     */
+    unfreeze(): void;
+    frozenClass: string;
+  }
+
+}
