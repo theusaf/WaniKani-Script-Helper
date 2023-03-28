ba11a7d9bc54e4bc02e5addcd0aa1b604c25c757
theusaf <theusafyt@gmail.com>
2023-03-28 12:05:41 -0700

    feat(controllers/user_synonyms): create typings

---
+++ src/types/wanikani/controllers/user_synonyms_controller.d.ts
@@ -0,0 +1,17 @@
+
+declare module "controllers/user_synonyms_controller" {
+  import { ValueDefinitionMap } from "@hotwired/stimulus/dist/types/core/value_properties";
+import { Controller } from "@hotwired/stimulus";
+  export default class extends Controller {
+    static targets: string[];
+    static values: ValueDefinitionMap;
+    get synonymTarget(): HTMLElement;
+    get synonymTargets(): HTMLElement[];
+    get hasSynonymTarget(): boolean;
+    get subjectIdValue(): number;
+    set subjectIdValue(value: number);
+    get hasSubjectIdValue(): boolean;
+    connect(): void;
+  }
+
+}
