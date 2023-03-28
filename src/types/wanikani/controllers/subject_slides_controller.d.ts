14b939d9cba93db89b98e8e42c90994a7d5b1c43
theusaf <theusafyt@gmail.com>
2023-03-28 11:59:28 -0700

    feat(controllers/subject_slides): create typings

---
+++ src/types/wanikani/controllers/subject_slides_controller.d.ts
@@ -0,0 +1,24 @@
+
+declare module "controllers/subject_slides_controller" {
+import AudioPlayerController from "controllers/audio_player_controller";
+  import { Controller } from "@hotwired/stimulus";
+  export default class extends Controller {
+    static targets: string[];
+    static outlets: string[];
+    get slideTarget(): HTMLElement;
+    get slideTargets(): HTMLElement[];
+    get hasSlideTarget(): boolean;
+    get navigationItemTarget(): HTMLElement;
+    get navigationItemTargets(): HTMLElement[];
+    get hasNavigationItemTarget(): boolean;
+    get audioPlayerOutlet(): AudioPlayerController;
+    get audioPlayerOutlets(): AudioPlayerController[];
+    get audioPlayerOutletElement(): HTMLElement;
+    get audioPlayerOutletElements(): HTMLElement[];
+    get hasAudioPlayerOutlet(): boolean;
+    connect(): void;
+    disconnect(): void;
+    switchSlide(event: Event): void;
+  }
+
+}
