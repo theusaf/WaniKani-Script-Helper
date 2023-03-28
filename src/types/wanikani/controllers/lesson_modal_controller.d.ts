c463f0da95e92b4684f7a0192ca4ec345ce382b1
theusaf <theusafyt@gmail.com>
2023-03-28 11:38:24 -0700

    feat(controllers/lesson_modal): create typings

---
+++ src/types/wanikani/controllers/lesson_modal_controller.d.ts
@@ -0,0 +1,18 @@
+declare module "controllers/lesson_modal_controller" {
+  import { Controller } from "@hotwired/stimulus";
+  export default class extends Controller {
+    static targets: string[];
+    get buttonTarget(): HTMLElement;
+    get buttonTargets(): HTMLElement[];
+    get hasButtonTarget(): boolean;
+    /**
+     * Enables modal mode.
+     */
+    connect(): void;
+    /**
+     * Disables modal mode.
+     */
+    disconnect(): void;
+  }
+
+}
