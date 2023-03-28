9af9793a22ea90fa5c7ed25d93c4ecd895e76022
theusaf <theusafyt@gmail.com>
2023-03-28 10:21:35 -0700

    feat(wanikani): create typings for autogrow controller

---
+++ src/types/wanikani/controllers/autogrow_controller.d.ts
@@ -0,0 +1,15 @@
+declare module "controllers/autogrow_controller" {
+  import { Controller } from "@hotwired/stimulus";
+
+  /**
+   * Auto-grows textareas.
+   */
+  export default class extends Controller {
+    initialize(): void;
+    connect(): void;
+    disconnect(): void;
+    autoGrow(): void;
+    debounce(func: () => void, wait: number): () => void;
+  }
+
+}
