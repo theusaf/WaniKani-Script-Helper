c0d294068da2dd2a170e323ed0828fbee82403fb
theusaf <theusafyt@gmail.com>
2023-03-28 11:47:28 -0700

    feat(controllers/search): create typings

---
+++ src/types/wanikani/controllers/search_controller.d.ts
@@ -0,0 +1,32 @@
+
+declare module "controllers/search_controller" {
+ import { ValueDefinitionMap } from "@hotwired/stimulus/dist/types/core/value_properties";
+ import { Controller } from "@hotwired/stimulus";
+  export default class extends Controller {
+    static values: ValueDefinitionMap;
+    static classes: string[];
+    static targets: string[];
+    get connectedValue(): boolean;
+    set connectedValue(value: boolean);
+    get hasConnectedValue(): boolean;
+    get openClass(): string;
+    get openClasses(): string;
+    get hasOpenClass(): boolean;
+    get searchFormTarget(): HTMLElement;
+    get searchFormTargets(): HTMLElement[];
+    get hasSearchFormTarget(): boolean;
+    get toggleTarget(): HTMLElement;
+    get toggleTargets(): HTMLElement[];
+    get hasToggleTarget(): boolean;
+    get inputTarget(): HTMLInputElement;
+    get inputTargets(): HTMLInputElement[];
+    get hasInputTarget(): boolean;
+    connect(): void;
+    disconnect(): void;
+    /**
+     * Toggles the search form.
+     */
+    toggle(): void;
+  }
+
+}
