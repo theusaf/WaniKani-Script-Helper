d283bd134f3c12a28e010e828016024a830f24ab
theusaf <theusafyt@gmail.com>
2023-03-28 08:15:29 -0700

    feat(controllers/quiz_queue/api): create typing

---
+++ src/types/wanikani/controllers/quiz_queue/api.d.ts
@@ -0,0 +1,17 @@
+declare module "controllers/quiz_queue/api" {
+
+  export default class API {
+
+    /**
+     * The CSRF token for the current session.
+     */
+    static get CSRFToken(): string;
+
+    /**
+     * The headers to use for JSON requests.
+     */
+    static get jsonHeaders(): Record<string, string>;
+
+  }
+
+}
