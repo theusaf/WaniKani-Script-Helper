9237ca0b301d8c2f4f9d85badf5d7a3bd4e1cfc3
theusaf <theusafyt@gmail.com>
2023-03-28 12:02:01 -0700

    feat(controllers/timeout): create typings

---
+++ src/types/wanikani/controllers/timeout_controller.d.ts
@@ -0,0 +1,9 @@
+declare module "controllers/timeout_controller" {
+  import { Controller } from "@hotwired/stimulus";
+  export default class extends Controller {
+    connect(): void;
+    disconnect(): void;
+    showConnectionTimeout(): void;
+  }
+
+}
