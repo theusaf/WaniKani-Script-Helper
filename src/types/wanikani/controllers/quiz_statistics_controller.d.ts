0c208ca1221c2d91e230873c34f8f4716087666e
theusaf <theusafyt@gmail.com>
2023-03-28 11:45:46 -0700

    feat(controllers/quiz_statistics): create typings

---
+++ src/types/wanikani/controllers/quiz_statistics_controller.d.ts
@@ -0,0 +1,18 @@
+declare module "controllers/quiz_statistics_controller" {
+  import { Controller } from "@hotwired/stimulus";
+  export default class extends Controller {
+    static targets: string[];
+    get percentCorrectTarget(): HTMLElement;
+    get percentCorrectTargets(): HTMLElement[];
+    get hasPercentCorrectTarget(): boolean;
+    get completeCountTarget(): HTMLElement;
+    get completeCountTargets(): HTMLElement[];
+    get hasCompleteCountTarget(): boolean;
+    get remainingCountTarget(): HTMLElement;
+    get remainingCountTargets(): HTMLElement[];
+    get hasRemainingCountTarget(): boolean;
+    connect(): void;
+    disconnect(): void;
+  }
+
+}
