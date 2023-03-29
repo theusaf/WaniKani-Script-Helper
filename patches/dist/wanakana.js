c434aae1a0ce47828f5994309d111e3c3d8fa935
theusaf <theusafyt@gmail.com>
2023-03-29 13:51:49 -0700

    chore: lazily add typing to wanakana script
    - the result is a kinda ugly semi-correctly-typed
      lots of any, mess of half-generated code

--- patches/dist/wanakana.js
+++
@@ -1,7 +0,0 @@
-export function typeOf(object) {
-    return null === object
-        ? "null"
-        : object !== Object(object)
-            ? typeof object
-            : {}.toString.call(object).slice(8, -1).toLowerCase();
-}
---
+++ src/patches/src/utils/number_to_words.ts
@@ -0,0 +1,14 @@
+export const words = [
+  "zero",
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+  "ten",
+];
+export default (i: number) => words[i] || i;
