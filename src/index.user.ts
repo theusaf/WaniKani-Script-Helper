30d9c129ac44706864f5fee88f53bd05ff623843
theusaf <theusafyt@gmail.com>
2023-03-28 19:07:47 -0700

    refactor: organize code

--- src/index.user.ts
+++ src/index.user.ts
@@ -14,54 +14,56 @@
 
 import { EventListenerCallback, WaniKaniEvents } from "./types/window";
 
-enum Events {
-  /**
-   * Fired when the connection is lost.
-   */
-  ConnectionTimeout = "connectionTimeout",
-  /**
-   * Fired when a question is answered.
-   */
-  QuestionAnswered = "didAnswerQuestion",
-  /**
-   * Fired when a subject is completed.
-   */
-  SubjectCompleted = "didCompleteSubject",
-  /**
-   * Fired when the SRS level of a subject changes.
-   */
-  SRSChanged = "didChangeSRS",
-  /**
-   * Fired when the user's synonyms are updated.
-   */
-  UserSynonymsUpdated = "didUpdateUserSynonyms",
-  /**
-   * Fired when a wrap-up observer is registered.
-   */
-  WrapUpObserverRegistration = "registerWrapUpObserver",
-  /**
-   * Fired when the quiz progress is updated.
-   */
-  QuizProgressUpdated = "updateQuizProgress",
-  /**
-   * Fired when the next question is about to be displayed.
-   */
-  NextQuestionWillDisplay = "willShowNextQuestion",
-  /**
-   * Fired when audio is about to play.
-   */
-  AudioWillPlay = "audioWillPlay",
-  /**
-   * Fired when content is about to be opened.
-   */
-  ContentWillOpen = "willOpenContent",
-}
-
 (function () {
   const wkhfListeners = new Map<
     keyof WaniKaniEvents,
     Set<EventListenerCallback<keyof WaniKaniEvents>>
   >();
+
+  // ———————————————————————————— Public Items ————————————————————————————
+
+  enum Events {
+    /**
+     * Fired when the connection is lost.
+     */
+    ConnectionTimeout = "connectionTimeout",
+    /**
+     * Fired when a question is answered.
+     */
+    QuestionAnswered = "didAnswerQuestion",
+    /**
+     * Fired when a subject is completed.
+     */
+    SubjectCompleted = "didCompleteSubject",
+    /**
+     * Fired when the SRS level of a subject changes.
+     */
+    SRSChanged = "didChangeSRS",
+    /**
+     * Fired when the user's synonyms are updated.
+     */
+    UserSynonymsUpdated = "didUpdateUserSynonyms",
+    /**
+     * Fired when a wrap-up observer is registered.
+     */
+    WrapUpObserverRegistration = "registerWrapUpObserver",
+    /**
+     * Fired when the quiz progress is updated.
+     */
+    QuizProgressUpdated = "updateQuizProgress",
+    /**
+     * Fired when the next question is about to be displayed.
+     */
+    NextQuestionWillDisplay = "willShowNextQuestion",
+    /**
+     * Fired when audio is about to play.
+     */
+    AudioWillPlay = "audioWillPlay",
+    /**
+     * Fired when content is about to be opened.
+     */
+    ContentWillOpen = "willOpenContent",
+  }
 
   function addEventListener<K extends keyof WaniKaniEvents>(
     event: K,
