73af65eea1dfe794cc81ee236b5487f92387097e
theusaf <theusafyt@gmail.com>
2023-04-05 18:13:27 -0700

    feat(TimeoutController): update typings
    + initialize()

--- src/types/wanikani/controllers/timeout_controller.d.ts
+++ src/types/wanikani/controllers/timeout_controller.d.ts
@@ -1,6 +1,7 @@
 declare module "controllers/timeout_controller" {
   import { Controller } from "@hotwired/stimulus";
   export default class extends Controller {
+    initialize(): void;
     connect(): void;
     disconnect(): void;
     showConnectionTimeout(): void;
--- wanikani/controllers/timeout_controller.js
+++ wanikani/controllers/timeout_controller.js
@@ -1,5 +1,8 @@
 import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
+  initialize() {
+    this.showConnectionTimeout = this.showConnectionTimeout.bind(this);
+  }
   connect() {
     window.addEventListener("connectionTimeout", this.showConnectionTimeout),
       (this.element.dataset.loaded = !0);
@@ -8,7 +11,7 @@ export default class extends Controller {
     window.removeEventListener("connectionTimeout", this.showConnectionTimeout),
       (this.element.dataset.loaded = !1);
   }
-  showConnectionTimeout = () => {
+  showConnectionTimeout() {
     (this.element.hidden = !1), this.dispatch("open");
-  };
+  }
 }
